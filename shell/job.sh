e25246ee30c562d3751ad7d15df070be2cfd8e3c
Angad P. Singh % Yufeng <aps2157@b1a.c2.titan>
2012-07-02 16:33:50 +0000

    Old scripts

---
+++ shell/job.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#$ -cwd
+
+/ifs/scratch/c2b2/ip_lab/aps2157/ExomePipe/shell/gatk_depthofcoverage.scr /ifs/scratch/c2b2/ip_lab/aps2157/yshendata/knome.aligned.bam.sorted.bam "1:10029-2524226"
+
---
+++ shell/job_snp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#$ -cwd
+
+/ifs/scratch/c2b2/ip_lab/aps2157/ExomePipe/shell/gatk_snpcall.scr /ifs/scratch/c2b2/ip_lab/aps2157/yshendata/knome.aligned.bam.sorted.bam
+
